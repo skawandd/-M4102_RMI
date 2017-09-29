13b921c3a66d9fb280238cf5c84946cc7e76b535
Dylan SKAWAND <dylan.skawand@etudiant.univ-lille1.fr>
2017-09-29 15:44:05 +0200

    test

---
+++ HelloWorldTCP/src/fr/univ_lille/iut/m4102/TalonCalculatrice.java
@@ -0,0 +1,33 @@
+package fr.univ_lille.iut.m4102;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class TalonCalculatrice implements Runnable {
+	private Socket unClient;
+	
+	public TalonCalculatrice(Socket unClient) {
+		this.unClient = unClient;
+	}
+
+	@Override
+	public void run() {
+		try {
+			PrintStream out = new PrintStream(unClient.getOutputStream());
+			BufferedReader in = new BufferedReader(new InputStreamReader(unClient.getInputStream()));
+			Calculatrice calc = new Calculatrice();
+			String s = in.readLine();
+			System.out.println("Le client " + unClient.getInetAddress() + " envoie : " + s);
+			calc.setS(s);
+			try {
+				out.println(calc.traitement());
+			} catch (DivisionParZero e) {
+				out.println(e.getMessage());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
